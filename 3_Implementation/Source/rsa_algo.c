664fe787cf4fd6fd47d6a427b6544be9ed2c4632
Shalini <85799635+shalini29112000@users.noreply.github.com>
2022-03-29 16:56:09 +0530

    Create rsa_algo.c

---
+++ 3_Implementation/Source/rsa_algo.c
@@ -0,0 +1,54 @@
+/**************************************************
+Two prime numbers are selected as p and q
+n = pq which is the modulus of both the keys.
+Calculate totient = (p-1)(q-1)
+Choose e such that e > 1 and coprime to totient which means gcd (e, totient) must be equal to 1, e is the public key
+Choose d such that it satisfies the equation de = 1 + k (totient), d is the private key not known to everyone.
+Cipher text is calculated using the equation c = m^e mod n where m is the message.
+With the help of c and d we decrypt message using equation m = c^d mod n where d is the private key.
+**************************************************/
+#include "modulars.h"
+#include "rsa.h"
+#include "primes.h"
+
+static long	pick_e(long fi)
+{
+  //Ensure a list of pirmes has been generated
+  //if length is 0, user didn't pass paramaters, use defined limit
+  if (g_prime_list.length == 0)
+    sieve_of_eratosthenes(RSA_SIEVE_LIMIT);
+
+  for (long i = 0; i < g_prime_list.length; i++)
+    {
+      //This will simply pick the first, not a great idea,
+      //but simple to understand algo
+      if (g_prime_list.primes[i] % fi != 0 && gcd(g_prime_list.primes[i], fi) == 1)
+	return g_prime_list.primes[i];
+    }
+
+  //Value must be greater than 1, so this is sufficent to indicate failure
+  return 0;
+}
+
+long		rsa_encrypt(long msg, long e, long n)
+{
+  return right_to_left(msg, e, n);
+}
+
+long		rsa_decrypt(long cyphertext, long d, long n)
+{
+  return right_to_left(cyphertext, d, n);
+}
+
+t_rsa		rsa_keygen(long p, long q)
+{
+  t_rsa		ret = { 0 };
+
+  //Ensure that n >= 2**b where b is the number of bits per block
+  //Example if encrypting a char, 2**8 = 256, thus n >= 256
+  ret.n = p * q;
+  ret.fi = (p - 1) * (q - 1);
+  ret.e = pick_e(ret.fi);
+  ret.d = modular_inverse(ret.e, ret.fi);
+  return ret;
+}
