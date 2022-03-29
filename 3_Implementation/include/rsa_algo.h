022e80956587aa345a8442466b60adb8db18da2c
Shalini <85799635+shalini29112000@users.noreply.github.com>
2022-03-29 16:28:48 +0530

    Create rsa_algo.h

---
+++ 3_Implementation/include/rsa_algo.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+	RSA Algorithm
+	Result reminder: ((m**e)**d) % n = m
+	e: encryption, d: decryption, 
+  Encryption: ciphertext = message**e	% n
+	Decryption: (c**d == (m**e)**d == m )	% n
+	RSA Key Generation
+	p and q, two distrinct prime numbers
+	n = pq
+	fi is Euler's Totient Function
+	fi(n) = fi(p) * fi(q) = (p - 1) * (q - 1) = n - (p + q - 1)
+	chose e, the public key:
+		- 1 < e < fi(n)
+		- gcd(e, fi(n)) == 1 (i.e. e and fi(n) are coprime
+	chose d, the private key:
+		- d == e**-1 (mod fi(n))
+		-> d is the modular multiplicative inverse of e (modulo(fi(n)))
+*******************************************************************************/
+
+#ifndef __RSA__
+# define __RSA__
+# define	RSA_SIEVE_LIMIT 255
+
+typedef struct	s_rsa {
+  long		n;
+  long		fi;
+  long		e;
+  long		d;
+} t_rsa;
+
+t_rsa		rsa_keygen(long p, long q);
+
+long		rsa_encrypt(long msg, long e, long n);
+long		rsa_decrypt(long cyphertext, long d, long n);
+
+#endif
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
