3f67e1abd726bb1d4318f17f1568517c6086efb0
Shalini <85799635+shalini29112000@users.noreply.github.com>
2022-03-31 12:24:00 +0530

    Update and rename modular_contri_algo.c to modAlgo.c

--- 3_Implementation/Source/modAlgo.c
+++ 3_Implementation/Source/modAlgo.c
@@ -2,13 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "primes.h"
-t_primes	g_prime_list = { 0 };
+#include "primeAlgo.h"
+t_primes g_prime_list = { 0 };
 
-/**
-  Greatest Common Denominator: largest number that can devide two numbers
- **/
-long	gcd(long a, long b)
+long gcd(long a, long b)
 {
   if (b == 0)
     return a;
@@ -17,9 +14,9 @@ long	gcd(long a, long b)
 }
 
 /**
-  Simple memory effecient way to determine if a number is prime
+  To check whether the number is a prime in efficient way
  **/
-char		is_prime(long number)
+char is_prime(long number)
 {
   if (number < 2)
     return 0;
@@ -31,32 +28,32 @@ char		is_prime(long number)
   return 1;
 }
 
-void		sieve_of_eratosthenes(long limit)
+void	sieve_of_eratosthenes(long limit)
 {
-  char		*primes;
+  char		*primeAlgo;
   long		prime_optimization = (long)lround(sqrtl(limit)) + 1;
   long		prime_count = limit - 2; //0 and 1 are not primes, so substract them
 
   //Allocate an array for all numbers in limit - 0 means not prime, 1 means prime
   //In C: false == 0; true is any number not equal to 0
-  primes = malloc(limit * sizeof(char));
+  primeAlgo = malloc(limit * sizeof(char));
   //Suppose all of them are prime
-  memset(primes, 1, limit * sizeof(char));
+  memset(primeAlgo, 1, limit * sizeof(char));
 
   //Eliminate none-primes
-  primes[0] = 0;
-  primes[1] = 0;
+  primeAlgo[0] = 0;
+  primeAlgo[1] = 0;
   //Go through array of numbers and eliminate multiples - the none primes
   for (long i = 2; i < prime_optimization; i++)
     {
-      if (primes[i])
+      if (primeAlgo[i])
 	{
 	  //Set all multiples to false
 	  for (long j = i * i; j < limit; j += i)
 	    {
-	      if (primes[j])
+	      if (primeAlgo[j])
 		{
-		  primes[j] = 0;
+		  primeAlgo[j] = 0;
 		  prime_count--;
 		}
 	    }
@@ -64,19 +61,19 @@ void		sieve_of_eratosthenes(long limit)
     }
 
   if (g_prime_list.length != 0)
-    free(g_prime_list.primes);
+    free(g_prime_list.primeAlgo);
   g_prime_list.limit_used = limit;
   g_prime_list.length = prime_count;
-  g_prime_list.primes = malloc(prime_count * sizeof(long));
+  g_prime_list.primeAlgo = malloc(prime_count * sizeof(long));
   long j = 0;
   for (long i = 0; i < limit; i++)
     {
-      if (primes[i])
+      if (primeAlgo[i])
 	{
-	  g_prime_list.primes[j] = i;
+	  g_prime_list.primeAlgo[j] = i;
 	  j++;
 	}
     }
 
-  free(primes);
+  free(primeAlgo);
 }
